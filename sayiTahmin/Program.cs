8511409f7a6e5624a18e523647cc063999056d71
İREM AYTEN <126502980+iremayten6@users.noreply.github.com>
2023-07-25 20:26:15 +0300

    Add project files.

---
+++ sayiTahmin/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Random random = new Random();
+int rastgele_sayi = random.Next(1, 50);
+int tahmin;
+int sayac = 0;
+
+do
+{
+    Console.Write("Bir sayi giriniz: ");
+    tahmin = Convert.ToInt32(Console.ReadLine());
+    if (tahmin > rastgele_sayi)
+    {
+        Console.WriteLine("Sayıyı küçültün.");
+    }
+    else if (tahmin < rastgele_sayi)
+    {
+        Console.WriteLine("Sayıyı büyültün.");
+    }
+    sayac++;
+} while (tahmin != rastgele_sayi);
+Console.WriteLine("TEBRİKLER " + sayac + ". denemede sayıyı buldunuz !!");
+
+Console.ReadLine();
